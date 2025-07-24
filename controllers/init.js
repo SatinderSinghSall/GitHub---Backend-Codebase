a92352ebab441d23813fec2eadfd1bb2a7495f7a
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-07-24 13:06:30 +0530

    Implemented the 'Initializing Repository' command.

--- controllers/init.js
+++ controllers/init.js
@@ -1,5 +1,21 @@
+const fs = require("fs").promises;
+const path = require("path");
+
 async function initRepo() {
-  console.log("Initializing a new repository...");
+  const reportPath = path.resolve(process.cwd(), ".Satinder_GitHub");
+  const commitPath = path.join(reportPath, "commits");
+
+  try {
+    await fs.mkdir(reportPath, { recursive: true });
+    await fs.mkdir(commitPath, { recursive: true });
+    await fs.writeFile(
+      path.join(reportPath, "config.json"),
+      JSON.stringify({ bucket: process.env.S3_BUCKET_NAME })
+    );
+    console.log("Repository initialized successfully.");
+  } catch (error) {
+    console.log("Error Initializing repository:", error.message);
+  }
 }
 
 module.exports = { initRepo };
