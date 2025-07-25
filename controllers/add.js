12ccbfd2ea6051f4c58f7cc9922e244904175e4b
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-07-25 20:18:14 +0530

    Implemented the 'Add Repository' command.

--- controllers/add.js
+++ controllers/add.js
@@ -4,10 +4,12 @@ const path = require("path");
 async function addFile(filePath) {
   const reportPath = path.resolve(process.cwd(), ".satinderGit");
   const stagingPath = path.join(reportPath, "staging");
+  const filename = path.basename(filePath);
+
   try {
-    await fs.mkdir(path.dirname(stagingPath), { recursive: true });
-    const filename = path.basename(filePath);
+    await fs.mkdir(stagingPath, { recursive: true });
     await fs.copyFile(filePath, path.join(stagingPath, filename));
+
     console.log(`File ${filename} added to the staging area.`);
   } catch (error) {
     console.log(`Error adding file ${filename}:`, error.message);
